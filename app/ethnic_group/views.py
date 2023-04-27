aab823d9d64599edbe981a6faa32346d98b2684c
samKenpachi011 <samuelkabelo1@gmail.com>
2023-04-27 13:28:50 +0200

    ADD: tests, upload image serializer and view

--- app/ethnic_group/views.py
+++ app/ethnic_group/views.py
@@ -1,7 +1,9 @@
 """
 View for the ethnic group
 """
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from ethnic_group import serializers
@@ -25,12 +27,27 @@ class EthnicGroupViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.EthnicGroupSerializer
+        elif self.action == 'upload_image':
+            return serializers.EthnicGroupImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new ethnic group."""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to the ethnic group."""
+
+        ethnic_group = self.get_object()
+        serializer = self.get_serializer(ethnic_group, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagsViewSet(mixins.DestroyModelMixin,
