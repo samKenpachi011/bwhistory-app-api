3b7d109d840b7faa83d6139f558dcb4e3463b3a0
samKenpachi011 <samuelkabelo1@gmail.com>
2023-05-10 16:54:38 +0200

    UPDATE: test to retrieve cultures for authenticated user

--- app/culture/tests/test_culture_api.py
+++ app/culture/tests/test_culture_api.py
@@ -5,10 +5,38 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model # noqa
+from django.contrib.auth import get_user_model
+from core.models import Culture, EthnicGroup
+from culture.serializers import CultureSerializer
 
 
 CULTURE_URL = reverse('culture:culture-list')
+
+
+def create_culture(user, **params):
+    """Create a new culture"""
+    group_defaults = {
+        'name': 'Tswana',
+        'description': 'The Tswana are a Bantu-speaking ethnic group',
+        'language': 'Setswana',
+        'population': 100,
+        'geography': 'Botswana',
+        'history': 'A brief history of the Tswana ethnic group.'
+    }
+
+    ethnic_group = EthnicGroup.objects.create(
+        user=user,
+        **group_defaults
+    )
+
+    defaults = {
+        'name': 'Test Culture',
+        'description': 'Test description',
+        'ethnic_group': ethnic_group
+    }
+
+    culture = Culture.objects.create(user=user, **defaults)
+    return culture
 
 
 class PublicCultureTests(TestCase):
@@ -21,3 +49,31 @@ class PublicCultureTests(TestCase):
         res = self.client.get(CULTURE_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateCultureTests(TestCase):
+    """Tests for authenticated users"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user = get_user_model().objects.create_user(
+            email='testuser@example.com',
+            password='testpassword')
+
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_culture(self):
+        """Test for retrieving culture information"""
+
+        create_culture(self.user)
+        create_culture(self.user)
+
+        res = self.client.get(CULTURE_URL)
+
+        cultures = Culture.objects.all().order_by('-id')
+
+        serializer = CultureSerializer(cultures, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
